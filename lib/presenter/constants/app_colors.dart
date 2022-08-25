6e3d2056a69a83613ee80bd67214a99790c95af6
Techi Samudio <mtsamudiog@gmail.com>
2022-08-25 14:09:47 -0400

    UI(app_colors.dart): Create app colors constants

---
+++ lib/presenter/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFF6750A4);
+  static const primary90 = Color(0xFFEADDFF);
+  static const primary95 = Color(0xFFF6EDFF);
+  static const textColor = Color(0xFF625B71);
+}
