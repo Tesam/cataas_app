003574469bf44311b60eef7b7fdc22a6bc2a1f17
Techi Samudio <mtsamudiog@gmail.com>
2022-08-26 01:23:58 -0400

    refactor(api url): Create a constant with the api base url

---
+++ lib/data/constants/api_constants.dart
@@ -0,0 +1,1 @@
+const apiUrl = "https://cataas.com/cat/gif";
--- lib/data/datasources/remote/cat_service.dart
+++ lib/data/datasources/remote/cat_service.dart
@@ -1,11 +1,12 @@
 import 'dart:typed_data';
+import 'package:cataas_app/data/constants/api_constants.dart';
 
 class CatService {
   CatService(this._client);
   final _client;
 
   Future<Uint8List?> getCat() async{
-    var response = await _client.get(Uri.parse('https://cataas.com/cat/gif'));
+    var response = await _client.get(Uri.parse(apiUrl));
     if (response.statusCode == 200) {
       return response.bodyBytes;
     }else{
