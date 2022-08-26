9ab46e20b9bb817333b17ece36cbab7fcbdbf06b
Techi Samudio <mtsamudiog@gmail.com>
2022-08-26 03:01:17 -0400

    UI(app_colors.dart): Add un selected color

--- lib/presenter/constants/app_colors.dart
+++ lib/presenter/constants/app_colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const primary95 = Color(0xFFEADDFF);
   static const primary90 = Color(0xFFF6EDFF);
   static const textColor = Color(0xFF625B71);
+  static const unSelectedColor = Color(0xFFF2F2F2);
 }
