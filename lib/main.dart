587cbab4a7456dfd9dc62c35d95000046ce103af
Techi Samudio <mtsamudiog@gmail.com>
2022-08-26 01:25:53 -0400

    feat(api url): Expose the dependency injector in the whole tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,23 @@
+import 'package:cataas_app/injector_widget.dart';
 import 'package:cataas_app/presenter/cat_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  var injector = InjectorWidget(child : const MyApp());
+  injector.init();
+
+  runApp(injector);
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const CatScreen(),
+    return const MaterialApp(
+      title: 'Cat Factory',
+      home: CatScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
